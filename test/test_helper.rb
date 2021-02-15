558cfe3e9e9b4a745685a4bebc552f23fdf15d95
Dan Milne <d@nmilne.com>
2021-02-15 12:02:50 +1100

    Do a test

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,5 +2,12 @@
 
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "rakuten_product_api"
-
 require "minitest/autorun"
+
+class MockHttpResponse
+  attr_accessor :body, :code
+  def initialize(body)
+    @body = body
+    @code = '200'
+  end
+end
